74248c1d7ed94047e1035b90c87f17c5aaba9771
chino-devops <danielnwankwo95@gmail.com>
2021-07-15 00:33:17 +0000

    second

--- services/watch_api/app.py
+++ services/watch_api/app.py
@@ -11,11 +11,56 @@ def get_brand():
 
 @app.route('/get_material', methods=['GET'] )
 def get_material():
-    return random.choice(['18k_Gold', '914L_steel', 'platinum', 'titanium'])
+    return random.choice(['18k_gold', '914L_steel', 'platinum', 'titanium'])
 
-# animal generator route here
+@app.route('/gen_cost', methods=['post'] )
+def gen_cost():
+    data = request.data.decode('utf-8')
+    data = data.split(' ')
+    brand = data[0]
+    material = data[1]
 
-# animal noise generator route here
+
+
+    if brand == 'rolex':
+        if material == '18k_gold':
+            return '£39,000'
+        elif material == '914L_steel':
+            return '£5,900'
+        elif material == 'platinum':
+            return '£31,590'
+        elif material == 'titanium':
+            return '£25,900'
+    elif brand == 'omega':
+        if material == '18k_gold':
+            return '£25,900'
+        elif material == '914L_steel':
+            return '£3,900'
+        elif material == 'platinum':
+            return '£21,590'
+        elif material == 'titanium':
+            return '£18,900'
+    elif brand == 'vacheron_constatin':
+        if material == '18k_gold':
+            return '£39,590'
+        elif material == '914L_steel':
+            return '£19,900'
+        elif material == 'platinum':
+            return '£28,590'
+        elif material == 'titanium':
+            return '£21,900'
+    elif brand == 'audemars_piguet':
+        if material == '18k_gold':
+            return '£49,590'
+        elif material == '914L_steel':
+            return '£5,900'
+        elif material == 'platinum':
+            return '£31,590'
+        elif material == 'titanium':
+            return '£25,900'
+    else:
+        return "please enter a correct choice" 
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
